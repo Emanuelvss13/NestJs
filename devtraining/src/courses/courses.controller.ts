e71051146378aa2a970875514483f9a0704264d2
Emanuelvss13 <emanuelsouza2002@gmail.com>
2021-08-09 10:42:18 -0300

    aula 04: work with route params

--- devtraining/src/courses/courses.controller.ts
+++ devtraining/src/courses/courses.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
-  @Get()
+  @Get('list')
   findAll() {
     return 'lista de cursos';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Curso #${id}`;
   }
 }
