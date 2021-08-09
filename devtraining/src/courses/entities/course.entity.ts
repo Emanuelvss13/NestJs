4708fdc1cac351115bfacfe23cdfd2d1545b2363
Emanuelvss13 <emanuelsouza2002@gmail.com>
2021-08-09 16:58:18 -0300

    aula 16: typeorm-entity

--- devtraining/src/courses/entities/course.entity.ts
+++ devtraining/src/courses/entities/course.entity.ts
@@ -1,7 +1,13 @@
-/* eslint-disable prettier/prettier */
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('courses')
 export class Course {
+  @PrimaryGeneratedColumn()
   id: number;
+  @Column()
   name: string;
+  @Column()
   description: string;
+  @Column('json', { nullable: true })
   tags: string[];
 }
